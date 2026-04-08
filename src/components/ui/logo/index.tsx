2961d352cc3700c9f1927d67d8818af823969071
Denys Pavlenko <de.pavlenko@gmail.com>
2026-04-08 10:07:55 +0300

    feat(app): fix logos

--- src/components/ui/logo/index.tsx
+++ src/components/ui/logo/index.tsx
@@ -7,16 +7,16 @@ export const Logo = ({ className }: { className?: string }) => {
         className={`hidden dark:block ${className}`}
         src="/logo-icon-light.svg"
         alt="app logo"
-        width={80}
-        height={80}
+        width={100}
+        height={100}
         priority
       />
       <Image
         className={`block dark:hidden ${className}`}
         src="/logo-icon-dark.svg"
         alt="app logo"
-        width={80}
-        height={80}
+        width={100}
+        height={100}
         priority
       />
     </>
